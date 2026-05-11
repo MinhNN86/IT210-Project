4e5ede479d297ae1087849b17103c7e2c060a406
MinhNN <minhnguyennhatt@gmail.com>
2026-05-11 22:18:30 +0700

    fix bug video call

--- src/main/java/com/projectit210/config/WebSocketConfig.java
+++ src/main/java/com/projectit210/config/WebSocketConfig.java
@@ -2,14 +2,24 @@ package com.projectit210.config;
 
 import com.projectit210.websocket.AuthHandshakeInterceptor;
 import com.projectit210.websocket.MeetingSignalingHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(128 * 1024);
+        container.setMaxBinaryMessageBufferSize(128 * 1024);
+        return container;
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
